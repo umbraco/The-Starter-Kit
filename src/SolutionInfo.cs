1cb4c195852c9c5653633cc1448b82d63d16ad5c
Shannon <sdeminick@gmail.com>
2017-08-07 15:53:47 +1000

    bumps version

--- src/SolutionInfo.cs
+++ src/SolutionInfo.cs
@@ -11,6 +11,6 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
-[assembly: AssemblyVersion("1.0.1")]
-[assembly: AssemblyFileVersion("1.0.1")]
-[assembly: AssemblyInformationalVersion("1.0.1")]
+[assembly: AssemblyVersion("1.0.2")]
+[assembly: AssemblyFileVersion("1.0.2")]
+[assembly: AssemblyInformationalVersion("1.0.2")]
