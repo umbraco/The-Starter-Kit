b2310b0cb6f2961fac2708b6840ac0ae2af56ed4
Warren Buckley <warren@umbraco.com>
2017-10-11 12:57:45 +0100

    Adds in the JS clientside parts to load the lessons in the help drawer

---
+++ src/Umbraco.SampleSite.Website/App_Plugins/StarterKit/lessons.service.js
@@ -0,0 +1,33 @@
+﻿angular.module('umbraco.services').factory('lessonsService', function ($http, $q, umbRequestHelper) {
+       
+    var service = {
+            
+        getLessons: function (path, baseurl) {
+
+            //build request values with optional params
+            var qs = "?path=" + path;
+            if (baseurl) {
+                qs += "&baseurl=" + encodeURIComponent(baseurl);
+            }
+
+            var url = umbRequestHelper.getApiUrl("lessonsApiBaseUrl", "GetLessons" + qs);
+
+            return umbRequestHelper.resourcePromise($http.get(url), "Failed to get lessons content");
+        },
+
+        getLessonSteps: function (path, baseurl) {
+
+            var qs = "?path=" + path;
+            if (baseurl) {
+                qs += "&baseurl=" + encodeURIComponent(baseurl);
+            }
+
+            var url = umbRequestHelper.getApiUrl("lessonsApiBaseUrl", "GetLessonSteps" + qs);
+
+            return umbRequestHelper.resourcePromise($http.get(url), "Failed to get lessons content");
+        }
+    };
+
+    return service;
+
+});
