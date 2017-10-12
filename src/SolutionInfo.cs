5333e5dff129146f681b4273dc895ba695853601
Warren Buckley <warren@umbraco.com>
2017-10-12 15:44:56 +0100

    Make the SK a v3.0.0 build which depends on 7.8.0 for this lessons stuff to appear in the new help drawer thats only in 7.8

--- src/SolutionInfo.cs
+++ src/SolutionInfo.cs
@@ -11,6 +11,6 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
-[assembly: AssemblyVersion("1.0.3")]
-[assembly: AssemblyFileVersion("1.0.3")]
-[assembly: AssemblyInformationalVersion("1.0.3")]
+[assembly: AssemblyVersion("3.0.0")]
+[assembly: AssemblyFileVersion("3.0.0")]
+[assembly: AssemblyInformationalVersion("3.0.0")]
