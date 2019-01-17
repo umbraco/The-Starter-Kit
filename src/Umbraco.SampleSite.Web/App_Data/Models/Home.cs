ab290d85a5c7c55f028a8a5e976646f2d9a13874
elitsa <elm@umbraco.dk>
2019-01-17 09:04:13 +0100

    Created a custom partial class for custom properties. Refactored implementation to apply a fix for a bug.

---
+++ src/Umbraco.SampleSite.Web/App_Data/Models/Home.cs
@@ -0,0 +1,30 @@
+﻿using System.Linq;
+using Umbraco.Core.PropertyEditors;
+
+namespace Umbraco.Web.PublishedModels
+{
+    public partial class Home
+    {
+        public string FontString
+        {
+            get
+            {
+                var p = GetProperty("font");
+                var o = p.PropertyType.DataType.Configuration;
+                var configuration = ConfigurationEditor.ConfigurationAs<ValueListConfiguration>(o);
+                return configuration.Items.First(x => x.Id == p.Value<int>()).Value;
+            }
+        }
+
+        public string ColorThemeString
+        {
+            get
+            {
+                var p = GetProperty("colorTheme");
+                var o = p.PropertyType.DataType.Configuration;
+                var configuration = ConfigurationEditor.ConfigurationAs<ValueListConfiguration>(o);
+                return configuration.Items.First(x => x.Id == p.Value<int>()).Value;
+            }
+        }
+    }
+}
