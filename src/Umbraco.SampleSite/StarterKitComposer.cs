775be10b987341e7b91cddebdbbb06f36b36132b
Elitsa Marinovska <elm@umbraco.dk>
2021-04-21 13:43:50 +0200

    From events to notifications

--- src/Umbraco.SampleSite/StarterKitComposer.cs
+++ src/Umbraco.SampleSite/StarterKitComposer.cs
@@ -1,18 +1,22 @@
 ﻿using System.Text.RegularExpressions;
-using Umbraco.Core.Composing;
-using Umbraco.Core.DependencyInjection;
-using Umbraco.Web.Tour;
+using Umbraco.Cms.Core.Composing;
+using Umbraco.Cms.Core.DependencyInjection;
+using Umbraco.Cms.Core.Events;
+using Umbraco.Cms.Core.Tour;
+using Umbraco.Cms.Infrastructure.WebAssets;
+using Umbraco.Extensions;
 
 namespace Umbraco.SampleSite
 {
-    public class StarterKitComposer : IUserComposer
+    public class StarterKitComposer : IComposer
     {
         public void Compose(IUmbracoBuilder builder)
         {
             //disable some of the default core tours since they don't make sense to have when the starter kit is installed
             builder.TourFilters().AddFilter(BackOfficeTourFilter.FilterAlias(new Regex("umbIntroCreateDocType|umbIntroCreateContent|umbIntroRenderInTemplate|umbIntroViewHomePage|umbIntroMediaSection")));
             
-            builder.Components().Append<StarterKitComponent>();
+            builder.AddNotificationHandler<ServerVariablesParsing, StarterKitNotificationHandler>();
+            builder.AddNotificationHandler<ImportedPackageNotification, StarterKitNotificationHandler>();
             builder.Services.AddUnique<FormsInstallationHelper>();
         }
     }
---
+++ src/Umbraco.SampleSite/StarterKitNotificationHandler.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.AspNetCore.Routing;
+using Umbraco.Cms.Core.Events;
+using Umbraco.Cms.Infrastructure.WebAssets;
+using Umbraco.Extensions;
+using Umbraco.SampleSite.Controllers;
+
+namespace Umbraco.SampleSite
+{
+    public class StarterKitNotificationHandler : INotificationHandler<ImportedPackageNotification>, INotificationHandler<ServerVariablesParsing>
+    {
+        private readonly FormsInstallationHelper _formsInstallationHelper;
+        private readonly LinkGenerator _linkGenerator;
+
+        public StarterKitNotificationHandler(FormsInstallationHelper formsInstallationHelper, LinkGenerator linkGenerator)
+        {
+            _formsInstallationHelper = formsInstallationHelper;
+            _linkGenerator = linkGenerator;
+        }
+
+        /// <summary>
+        /// Handles the <see cref="ServerVariablesParsing"/> notification to add custom urls
+        /// </summary>
+        public void Handle(ServerVariablesParsing notification)
+        {
+            IDictionary<string, object> serverVars = notification.ServerVariables;
+
+            if (!serverVars.ContainsKey("umbracoUrls"))
+            {
+                throw new ArgumentException("Missing umbracoUrls.");
+            }
+            
+            var umbracoUrlsObject = serverVars["umbracoUrls"];
+            if (umbracoUrlsObject == null)
+            {
+                throw new ArgumentException("Null umbracoUrls");
+            }
+
+            if (!(umbracoUrlsObject is Dictionary<string, object> umbracoUrls))
+            {
+                throw new ArgumentException("Invalid umbracoUrls");
+            }
+
+            //Add to 'Umbraco.Sys.ServerVariables.umbracoUrls.lessonsApiBaseUrl' global JS object
+            //The URL/route for this API endpoint to be consumed by the Lessons AngularJS Service
+            umbracoUrls["lessonsApiBaseUrl"] = _linkGenerator.GetUmbracoApiServiceBaseUrl<LessonsController>(controller => controller.GetLessons(""));
+        }
+
+        public void Handle(ImportedPackageNotification notification)
+        {
+            if (notification?.PackageMetaData != null && notification.PackageMetaData.Name == "Umbraco Forms")
+            {
+                _formsInstallationHelper.UpdateUmbracoDataForFormsInstallation();
+            }
+        }
+    }
+}
