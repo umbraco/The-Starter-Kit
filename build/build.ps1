13579d579ec25b9d1ca8eef0c275b72a2704f8d1
Shannon <sdeminick@gmail.com>
2021-05-12 15:54:37 -0700

    Moves the app-plugins files to the non-legacy project and gets the build script working

--- build/build.ps1
+++ build/build.ps1
@@ -56,66 +56,33 @@ If ($FileExists -eq $False) {
 	Invoke-WebRequest $SourceNugetExe -OutFile $NuGet
 }
 
-# ensure we have vswhere
-New-Item "$BuildFolder\vswhere" -type directory -force
-$vswhere = "$BuildFolder\vswhere.exe"
-if (-not (test-path $vswhere))
-{
-   Write-Host "Download VsWhere..."
-   $path = "$BuildFolder\tmp"
-   &$nuget install vswhere -OutputDirectory $path -Verbosity quiet
-   $dir = ls "$path\vswhere.*" | sort -property Name -descending | select -first 1
-   $file = ls -path "$dir" -name vswhere.exe -recurse
-   mv "$dir\$file" $vswhere   
- }
-
-$MSBuild = &$vswhere -latest -requires Microsoft.Component.MSBuild -find MSBuild\**\Bin\MSBuild.exe | select-object -first 1
-if (-not (test-path $MSBuild)) {
-    throw "MSBuild not found!"
-}
-
 if ((Get-Item $ReleaseFolder -ErrorAction SilentlyContinue) -ne $null)
 {
 	Write-Warning "$ReleaseFolder already exists on your local machine. It will now be deleted."
 	Remove-Item $ReleaseFolder -Recurse
 }
 
-
 ####### DO THE SLN BUILD PART #############
-
-# Set the version number in SolutionInfo.cs
-$SolutionInfoPath = Join-Path -Path $SolutionRoot -ChildPath "SolutionInfo.cs"
-(gc -Path $SolutionInfoPath) `
-	-replace "(?<=Version\(`")[.\d]*(?=`"\))", $ReleaseVersionNumber |
-	sc -Path $SolutionInfoPath -Encoding UTF8
-(gc -Path $SolutionInfoPath) `
-	-replace "(?<=AssemblyInformationalVersion\(`")[.\w-]*(?=`"\))", $FullVersionName |
-	sc -Path $SolutionInfoPath -Encoding UTF8
-# Set the copyright
-$Copyright = "Copyright " + [char]0x00A9 + " Umbraco " + (Get-Date).year;
-(gc -Path $SolutionInfoPath) `
-	-replace "(?<=AssemblyCopyright\(`").*(?=`"\))", $Copyright |
-	sc -Path $SolutionInfoPath -Encoding UTF8;
 
 # Build the solution in release mode
 $SolutionPath = Join-Path -Path $SolutionRoot -ChildPath "Umbraco.SampleSite.sln";
 
 #restore nuget packages
 Write-Host "Restoring nuget packages..."
-& $NuGet restore $SolutionPath
+& dotnet restore $SolutionPath
 
 # clean sln for all deploys
-& $MSBuild "$SolutionPath" /p:Configuration=Release /maxcpucount /t:Clean
+& dotnet clean "$SolutionPath" --configuration Release
 if (-not $?)
 {
-	throw "The MSBuild process returned an error code."
+	throw "The dotnet process returned an error code."
 }
 
 #build
-& $MSBuild "$SolutionPath" /p:Configuration=Release /maxcpucount
+& dotnet build "$SolutionPath" --configuration Release
 if (-not $?)
 {
-	throw "The MSBuild process returned an error code."
+	throw "The dotnet process returned an error code."
 }
 
 ####### DO THE UMBRACO PACKAGE BUILD #############
