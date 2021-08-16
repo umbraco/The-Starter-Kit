cd73bea1fbb61cb662323913c6706a3090aaf264
Bjarke Berg <mail@bergmania.dk>
2021-08-16 12:41:39 +0200

    Updated to nightly

--- src/Umbraco.SampleSite/Migrations/ImportPackageXmlMigration.cs
+++ src/Umbraco.SampleSite/Migrations/ImportPackageXmlMigration.cs
@@ -1,6 +1,9 @@
 ﻿using System.Xml.Linq;
+using Umbraco.Cms.Core.IO;
 using Umbraco.Cms.Core.Packaging;
+using Umbraco.Cms.Core.PropertyEditors;
 using Umbraco.Cms.Core.Services;
+using Umbraco.Cms.Core.Strings;
 using Umbraco.Cms.Infrastructure.Migrations;
 using Umbraco.Cms.Infrastructure.Packaging;
 
@@ -8,28 +11,27 @@ namespace Umbraco.SampleSite.Migrations
 {
     public class ImportPackageXmlMigration : PackageMigrationBase
     {
-        private XDocument _xdoc;
-
-        public ImportPackageXmlMigration(IPackagingService packagingService, IMigrationContext context) 
-            : base(packagingService, context)
+        public ImportPackageXmlMigration(
+            IPackagingService packagingService,
+            IMediaService mediaService,
+            MediaFileManager mediaFileManager, 
+            MediaUrlGeneratorCollection mediaUrlGenerators,
+            IShortStringHelper shortStringHelper, 
+            IContentTypeBaseServiceProvider contentTypeBaseServiceProvider, 
+            IMigrationContext context) 
+            : base(packagingService, 
+                mediaService,
+                mediaFileManager, 
+                mediaUrlGenerators, 
+                shortStringHelper, 
+                contentTypeBaseServiceProvider, 
+                context)
         {
         }
-        
-        private XDocument PackageDataManifest
-        {
-          get
-          {
-            if (this._xdoc != null)
-              return this._xdoc;
-            this._xdoc = PackageMigrationResource.GetEmbeddedPackageDataManifest(this.GetType());
-            return this._xdoc;
-          }
-        }
-
 
         protected override void Migrate()
         {
-            ImportPackage.FromXmlDataManifest(PackageDataManifest).Do();
+            ImportPackage.FromEmbeddedResource(GetType()).Do();
             Context.AddPostMigration<PublishRootBranchPostMigration>();
         }
     }
