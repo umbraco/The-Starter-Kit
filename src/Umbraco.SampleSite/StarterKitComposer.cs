1db3c49e372ec30eae2ea37465cbe208bdc0f70f
nikolajlauridsen <nikolajlauridsen@protonmail.ch>
2024-05-06 14:03:20 +0200

    Comment out tour filter which no longer exists

--- src/Umbraco.SampleSite/StarterKitComposer.cs
+++ src/Umbraco.SampleSite/StarterKitComposer.cs
@@ -1,8 +1,6 @@
 using System.Text.RegularExpressions;
 using Umbraco.Cms.Core.Composing;
 using Umbraco.Cms.Core.DependencyInjection;
-using Umbraco.Cms.Core.Notifications;
-using Umbraco.Cms.Core.Tour;
 using Umbraco.Cms.Infrastructure.Migrations.Notifications;
 using Umbraco.SampleSite.Migrations;
 
@@ -13,7 +11,8 @@ public partial class StarterKitComposer : IComposer
     public void Compose(IUmbracoBuilder builder)
     {
         // disable some of the default core tours since they don't make sense to have when the starter kit is installed
-        builder.TourFilters().AddFilter(BackOfficeTourFilter.FilterAlias(ToursToRemoveRegex()));
+        // TODO: Figure out how to do this in V14, if possible.
+        // builder.TourFilters().AddFilter(BackOfficeTourFilter.FilterAlias(ToursToRemoveRegex()));
 
         // builder.AddNotificationHandler<ServerVariablesParsingNotification, StarterKitNotificationHandler>();
         builder.AddNotificationHandler<MigrationPlansExecutedNotification, PostMigrationNotificationHandler>();
